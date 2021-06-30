ae1d2b3c88561a24f956b52856edf344bb692d2f
AleVazquezSolis <claudia.vazquez@essilorusa.com>
2021-06-29 22:34:40 -0600

    changes in PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -8,7 +8,7 @@ def generate_password():
     lowercase_letters=["a","b","c","d","e","f","g","h",
     "i","j","k","l","n","m","o","p","q","r","s","t","u",
     "v","w","x","y","z"]
-    symbols=["!","#","$","%","&","/","(",")","="]
+    symbols=["!","#","$","%","&","/"]
     numbers=["0","1","2","3","4","5","6","7","8","9"]
     password=[]
     characteres= uppercase_letters + lowercase_letters + symbols + numbers
